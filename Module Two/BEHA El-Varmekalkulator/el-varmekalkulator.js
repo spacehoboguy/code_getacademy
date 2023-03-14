5203652cf1a4ae0f15f009809333206a7037166d
Philip Olafsen <ph.olafsen@gmail.com>
2023-03-14 10:38:27 +0100

    stylesheet creation

--- Module Two/BEHA El-Varmekalkulator/el-varmekalkulator.js
+++
@@ -1,9 +0,0 @@
-function calcWatt (inputArea,effectOfRoom){
-let watt = inputArea*effectOfRoom;
-console.log(watt,"Watt")
-document.getElementById('display').innerHTML = `
-<tr>`;
-watt;
-`</tr>
-`;
-}
